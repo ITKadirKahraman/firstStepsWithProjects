cfe400ca3e1231be07d61dd1f627dadf03bbfd3d
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-05-02 23:56:40 +0200

    Add functions to initialize book card and handle comments

--- bookStore/script.js
+++ bookStore/script.js
@@ -1,1 +1,21 @@
+let initBookCard = document.getElementById('bookCard');
+const userName = document.getElementById('userNameComment');
 
+function initBook() {
+    initBookCard.innerHTML = "";
+    for (let indexBook = 0; indexBook < books.length; indexBook++) {
+        initBookCard.innerHTML += getCreatedBookCard[indexBook];   
+    }
+    getHeader();
+}
+
+function userNameInput() {
+    return userName = "Kadir";
+}
+
+function addComment() {
+    const commentInput = document.getElementById('inputComment');
+    const commentText = commentInput.value.trim();
+    books[index].comments.push(commentText);
+    commentText.value = "";
+}
