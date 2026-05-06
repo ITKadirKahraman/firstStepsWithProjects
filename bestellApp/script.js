737aa85be37e9059ec01b2d249db185e6d6ae6d0
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-05-06 13:46:12 +0200

    Add initialization and rendering functions

--- bestellApp/script.js
+++ bestellApp/script.js
@@ -1,1 +1,14 @@
+function init() {
+    renderNavigation();
+    renderHeader();
+}
 
+function renderNavigation() {
+    const nav = document.getElementById('nav');
+    nav.innerHTML += getNavigation();
+}
+
+function renderHeader() {
+    const header = document.getElementById('header');
+    header.innerHTML += getHeader();
+}
