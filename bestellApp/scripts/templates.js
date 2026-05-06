c7691bb1b733ee8046a10c2470777e5be8247ba9
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-05-06 13:45:57 +0200

    Add navigation and header template functions

--- bestellApp/scripts/templates.js
+++ bestellApp/scripts/templates.js
@@ -1,1 +1,26 @@
+function getNavigation() {
+    return `<header>
+            <div class="navContainer">
+                <img src="assets/icons/bestellAppLogo.svg" alt="Firmen Logo">
+                <img class="imgBar" src="assets/icons/bars.svg" alt="Menüleiste">
+            </div>
+    </header>`;
+}
 
+function getHeader() {
+    return `<section class="headerContainer">
+        <div class="headerImg"> <img src="assets/img/header.jpg" alt="Titelbild"></div>
+        <div class="circleImg"><img src="assets/img/logoBurger.png"></div>
+        <div class="headerInformation">
+            <div class="headerTitle">
+                <h1 class="burgerText"><span>Burger</span>House</h1>
+                <div class="starContainer">
+                    <img class="star" src="assets/icons/star.svg">
+                    <p>4,1</p>
+                    <p>(317)</p>
+                </div>
+            </div>
+            <p class="HeaderText">The best of Burgers, Pizza, and Greens, all in one great place.</p>
+        </div>
+    </section>`;
+}
