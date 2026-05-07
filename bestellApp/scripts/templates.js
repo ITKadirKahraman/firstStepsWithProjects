c8ea114b5aaefc542053dc9966ecf0239adaea9d
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-05-07 23:18:21 +0200

    Refactor header and category separator templates
    Updated HTML structure and class names in template functions for better consistency and clarity.

--- bestellApp/scripts/templates.js
+++ bestellApp/scripts/templates.js
@@ -9,18 +9,96 @@ function getNavigation() {
 
 function getHeader() {
     return `<section class="headerContainer">
-        <div class="headerImg"> <img src="assets/img/header.jpg" alt="Titelbild"></div>
-        <div class="circleImg"><img src="assets/img/logoBurger.png"></div>
+        <div class="headerImg"></div>
+        <div class="circle"><img class="circleImg" src="assets/img/logoBurger.png"></div>
         <div class="headerInformation">
             <div class="headerTitle">
                 <h1 class="burgerText"><span>Burger</span>House</h1>
                 <div class="starContainer">
                     <img class="star" src="assets/icons/star.svg">
-                    <p>4,1</p>
-                    <p>(317)</p>
+                    <p class="ranting">4,1</p>
+                    <p class="commentsNumber">(317)</p>
                 </div>
             </div>
-            <p class="HeaderText">The best of Burgers, Pizza, and Greens, all in one great place.</p>
+            <p class="headerText">The best of Burgers, Pizza, and Greens, all in one great place.</p>
         </div>
     </section>`;
+}
+
+function getSeperatorBurger() {
+    return `<div class="categorySeperator"></div>    
+        <div class="h2Container">
+            <img class="categoryIcon burgerIcon" src="assets/icons/burger.svg" alt="Burger">
+            <h2>Burger</h2>
+        </div>
+    `;
+}
+
+function getSeperatorPizza() {
+    return `<div class="categorySeperator"></div>    
+        <div class="h2Container">
+            <img class="categoryIcon burgerIcon" src="assets/icons/pizza.svg" alt="Pizza">
+            <h2>Pizza</h2>
+        </div>
+    `;
+}
+
+function getSeperatorSalad() {
+    return `<div class="categorySeperator"></div>    
+        <div class="h2Container">
+            <img class="categoryIcon burgerIcon" src="assets/icons/salad.svg" alt="Salad">
+            <h2>Salad</h2>
+        </div>
+    `;
+}
+
+function getBurger(index) {
+    return `<section class="sectionBurger">
+        <div class="card" id="card">
+            <img class="cardImg" src="assets/img/${burger[index].image}.svg" alt="Burger">
+            <p class="cardHeader">${burger[index].name}</p>
+            <p class="cardSubtext">${burger[index].description}</p>
+            <div class="priceAndAdd">
+                <p class="price"<${burger[index].price}</p>
+                <button class="btnAddBasket" id="btnAddBasket" onclick="addToBasket(index)">
+                    <img class="addIcons" id="addIcons" src="assets/icons/${burger[index].added ? 'added' : 'addToBasket'}.svg" alt="Einkaufswagen">
+                </button>
+            </div>
+        </div>
+    </section>
+    `;
+}
+
+function getPizza(index) {
+    return `<section class="sectionBurger">
+        <div class="card" id="card">
+            <img class="cardImg" src="assets/img/${pizza[index].image}.svg" alt="Burger">
+            <p class="cardHeader">${pizza[index].name}</p>
+            <p class="cardSubtext">${pizza[index].description}</p>
+            <div class="priceAndAdd">
+                <p class="price"<${pizza[index].price}</p>
+                <button class="btnAddBasket" id="btnAddBasket" onclick="addToBasket(index)">
+                    <img class="addIcons" id="addIcons" src="assets/icons/${pizza[index].added ? 'added' : 'addToBasket'}.svg" alt="Einkaufswagen">
+                </button>
+            </div>
+        </div>
+    </section>
+    `;
+}
+
+function getSalad(index) {
+    return `<section class="sectionBurger">
+        <div class="card" id="card">
+            <img class="cardImg" src="assets/img/${salad[index].image}.svg" alt="Burger">
+            <p class="cardHeader">${salad[index].name}</p>
+            <p class="cardSubtext">${salad[index].description}</p>
+            <div class="priceAndAdd">
+                <p class="price"<${salad[index].price}</p>
+                <button class="btnAddBasket" id="btnAddBasket" onclick="addToBasket(index)">
+                    <img class="addIcons" id="addIcons" src="assets/icons/${salad[index].added ? 'added' : 'addToBasket'}.svg" alt="Einkaufswagen">
+                </button>
+            </div>
+        </div>
+    </section>
+    `;
 }
