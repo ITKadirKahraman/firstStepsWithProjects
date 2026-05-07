e1f787e980547ecc54349b3cb7035609e0e23a51
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-05-07 23:19:05 +0200

    Add rendering functions for burger, pizza, and salad

--- bestellApp/script.js
+++ bestellApp/script.js
@@ -1,6 +1,9 @@
 function init() {
     renderNavigation();
     renderHeader();
+    renderBurger();
+    renderPizza();
+    renderSalad();
 }
 
 function renderNavigation() {
@@ -11,4 +14,49 @@ function renderNavigation() {
 function renderHeader() {
     const header = document.getElementById('header');
     header.innerHTML += getHeader();
+}
+
+function renderBurgerSection() {
+    let content = getSeperatorBurger();
+
+    for (let index = 0; index < burger.length; index++) {
+        content += getBurger(index);
+    }
+
+    return content;
+}
+
+function renderBurger() {
+    const dishes = document.getElementById('burger');
+    dishes.innerHTML = renderBurgerSection();
+}
+
+function renderPizzaSection() {
+    let content = getSeperatorPizza();
+
+    for (let index = 0; index < burger.length; index++) {
+        content += getPizza(index);
+    }
+
+    return content;
+}
+
+function renderPizza() {
+    const dishes = document.getElementById('pizza');
+    dishes.innerHTML = renderPizzaSection();
+}
+
+function renderSaladSection() {
+    let content = getSeperatorSalad();
+
+    for (let index = 0; index < burger.length; index++) {
+        content += getSalad(index);
+    }
+
+    return content;
+}
+
+function renderSalad() {
+    const dishes = document.getElementById('salad');
+    dishes.innerHTML = renderSaladSection();
 }
