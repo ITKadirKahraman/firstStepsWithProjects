803e25b8a73360362d0907108b3170fb95e87bd6
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-05-19 23:22:05 +0200

    Add template functions for navigation and footer

--- bestellApp/scripts/templates.js
+++ bestellApp/scripts/templates.js
@@ -224,3 +224,177 @@ function order() {
         </div>
     </dialog>`;
 }
+
+
+
+
+
+function getNavigation() {
+    return `<header class="header">
+            <div class="navContainer">
+                <img src="assets/icons/bestellAppLogo.svg" alt="Firmen Logo">
+                <img class="imgBar" src="assets/icons/bars.svg" alt="Menüleiste">
+            </div>
+    </header>`;
+}
+
+function getHeader() {
+    return `<section class="headerContainer">
+        <div class="headerImg"></div>
+        <div class="circle"><img class="circleImg" src="assets/img/logoBurger.png"></div>
+        <div class="headerInformation">
+            <div class="headerTitle">
+                <h1 class="burgerText"><span>Burger</span>House</h1>
+                <div class="starContainer">
+                    <img class="star" src="assets/icons/star.svg">
+                    <p class="ranting">4,1</p>
+                    <p class="commentsNumber">(317)</p>
+                </div>
+            </div>
+            <p class="headerText" id="menu">The best of Burgers, Pizza, and Greens, all in one great place.</p>
+        </div>
+    </section>`;
+}
+
+function getSeparator(title, icon) {
+    return `<div class="categoryWrapper">
+            <div class="categorySeperator"></div>    
+            <div class="h2Container">
+                <img class="categoryIcon dishesIcon" src="assets/icons/${icon}.svg" alt="${title}">
+                <h2>${title}</h2>
+            </div>
+        </div>
+    `;
+}
+
+function getDishes(category, index, title) {
+    return `<section class="sectionBurger">
+        <div class="card" id="card">
+            <img class="cardImg" src="assets/img/${dishes[category][index].image}.jpg" alt="${title}">
+            <p class="cardHeader">${dishes[category][index].name}</p>
+            <p class="cardSubtext">${dishes[category][index].description}</p>
+            <div class="priceAndAdd">
+                <p class="price">${dishes[category][index].price.toFixed(2).toString().replace(".", ",")} <strong>€</strong></p>
+                <button class="btnAddBasket" id="btnAddBasket" onclick="addToBasket(dishes['${category}'][${index}])">
+                    <p class="basketText">${basket.some(item => item.name === dishes[category][index].name) ? 'Added' : 'Add to basket'}</p>
+                </button>
+            </div>
+        </div>
+    </section>`;
+}
+
+function getFooter() {
+    return `<footer>
+        <div class="footerContainer">
+            <nav class="footerNav">
+                <a id="topToBot">
+                    <img class="copyright" src="assets/icons/copyright.svg" alt="copyright">
+                    2026 BurgerHouse 
+                </a>
+                <a id="midToTop">
+                    Imprint
+                </a>
+                <a id="botToMid">
+                    Cookie Preferences
+                </a>
+            </nav>
+        </div>
+        <div class="subFooterContainer">
+            <a href="#start" class="start">
+                <img class="home" src="assets/icons/home.svg" alt="Home">
+            </a>
+            <button class="subFooterButton">
+                <img class="person" src="assets/icons/person.svg" alt="Profile">
+            </button>
+            <a href="#menu" class="menu">
+                <img class="takeout" src="assets/icons/takeout.svg" alt="Menu">
+            </a>
+            <button class="subFooterButton" id="openBasket" onclick="openBasket()">
+                <img class="shopping" src="assets/icons/shopping_cart.svg" alt="ShoppingCart">
+            </button>
+        </div>
+    </footer>`;
+}
+
+function basketTemplate() {
+    return `<div class="basketSection" id="basketSection">
+        <div class="closeContainer"> 
+            <button class="closeBtn" id="closeBtn" onclick="closeBasket()">
+                <img class="close" id="close" src="assets/icons/close.svg" alt="Close">
+            </button>
+        </div>
+        <h3>Your Basket</h3>
+        <div class="itemsScroll">
+            <div id="basketItems"></div>
+        </div>
+        <div id="calculator"></div>
+    </div>`;
+}
+
+function basketItemTemplate(item, index) {
+    return `<div class="basketCard" id="basketItem">
+        <p class="basketOrderDishes">${item.amount} x ${item.name}</p>
+        <div class="basketCardBotton">
+            <p class="switchTrashMinusPlus">
+                <button onclick="${item.amount === 1 ? `removeFromBasket(${index})` : `counter(${index}, -1)`}" class="removeCount">
+                    ${item.amount === 1 ? '<img class="trash" src="assets/icons/trash.svg" alt="Trash">' : '<img class="minus" src="assets/icons/minus.svg" alt="Minus">'}
+                </button>
+                <span class="amount">${item.amount}</span>
+                <button class="btnPlus" onclick="counter(${index}, 1)">
+                    <img class="plus" src="assets/icons/plus.svg" alt="Plus">
+                </button>
+            </p>
+            <p class="basketPrice">
+                ${(item.price * item.amount).toFixed(2).toString().replace(".", ",")} €
+            </p>
+        </div>
+    </div>`;
+}
+
+function calculator() {
+    return `<div class="calculator">
+        <table>
+            <tbody>
+                <tr>
+                    <td>Subtotal</td>
+                    <td class="tdEnd" id="subtotal"></td>
+                </tr>
+                <tr>
+                    <td>Delivery fee</td>
+                    <td class="tdEnd" id="delivery"></td>
+                </tr>
+                <tr>
+                    <th>Total</th>
+                    <th class="tdEnd" id="total"></th>
+                </tr>
+            </tbody>
+        </table>
+        <button id="btnBuyNow" class="btnBuyNow" onclick="openOrder()"></button>
+    </div>`;
+}
+
+function emptyBasket() {
+    return `<div class="basketSection">
+        <div class="basketIsEmpty">
+            <button class="closeBtn" id="closeBtn" onclick="closeBasket()">
+                <img class="close" id="close" src="assets/icons/close.svg" alt="Close">
+            </button>
+            <h3 class="emptyH3">Your Basket</h3>
+            <p class="emptyP">Nothing here yet. <br> Go ahead and choose <br> something delicious! </p>
+            <img class="emptyImg" src="assets/icons/basketShoppingCart.svg" alt="Shopping cart">
+        </div>
+    </div>`;
+}
+
+function order() {
+    return `<dialog class="orderContainer" id="orderContainer">
+        <button class="closeBtnOrder" id="closeBtn" onclick="closeOrder()">
+            <img id="close" src="assets/icons/close.svg" alt="Close">
+        </button>
+        <div class="orderText">
+            <img class="carImg" src="assets/icons/car.svg" alt="Car">
+            <h3>Order confirmed!</h3>
+            <p class="orderP">Your foor is on the way!</p>
+        </div>
+    </dialog>`;
+}
