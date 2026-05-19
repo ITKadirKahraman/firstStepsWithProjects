da6a0ebbb40b026c24e280e2aa802c7e934de13b
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-05-19 23:21:45 +0200

    Define dishes object with menu items
    Added dishes object with burgers, pizzas, and salads.

--- bestellApp/scripts/db.js
+++ bestellApp/scripts/db.js
@@ -92,3 +92,106 @@ let salad = [
 ]
 
 let basket = [];
+
+
+
+
+let dishes = {
+    burger : 
+    [
+        {
+            "image": "burgerBlack",
+            "name": "Veggie mushroom black burger",
+            "price": 16.90,
+            "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+            "added": false
+        },
+        {
+            "image": "baconBurger",
+            "name": "All meat burger",
+            "price": 15.90,
+            "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+            "added": false
+        },
+        {
+            "image": "redBurger",
+            "name": "Beef red burger",
+            "price": 14.90,
+            "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+            "added": false
+        },
+        {
+            "image": "chickenBurger",
+            "name": "Big chicken burger",
+            "price": 15.90,
+            "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+            "added": false
+        }
+    ],  
+
+    pizza :
+    [
+        {
+            "image": "cheese",
+            "name": "Pizza Margherita",
+            "price": 11.90,
+            "description": "Tomato Souce, Mozzarella",
+            "added": false
+        },
+        {
+            "image": "salami",
+            "name": "Pizza Chorizo",
+            "price": 13.90,
+            "description": "Tomato slices, Mozzarella, Chorizo",
+            "added": false
+        },
+        {
+            "image": "funghi",
+            "name": "Funghi",
+            "price": 12.90,
+            "description": "Red onion, Oliven, Button Mushrooms, Mozzarella",
+            "added": false
+        },
+        {
+            "image": "chicken",
+            "name": "Quattro Formaggi with Chicken",
+            "price": 15.90,
+            "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+            "added": false
+        }
+    ],
+
+ salad : 
+    [
+        {
+            "image": "argula",
+            "name": "Warm beef argula salad",
+            "price": 16.90,
+            "description": "Beef, Argula, Field salad, Greek feta, Cherry tomatoes, Sun-drief Tomatoes, Balsamicvinegar dressing",
+            "added": false
+        },
+        {
+            "image": "miniSalad",
+            "name": "Mini green Salad",
+            "price": 7.90,
+            "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+            "added": false
+        },
+        {
+            "image": "greenSaladSeaFood",
+            "name": "Green Salad with sea food",
+            "price": 16.90,
+            "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustardlemon dressing with dill",
+            "added": false
+        },
+        {
+            "image": "tofu",
+            "name": "Vegan green salad with tofu",
+            "price": 14.90,
+            "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+            "added": false
+        }
+    ]
+};
+
+let basket = [];
