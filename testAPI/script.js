2e7fd5bf3d4ae7356ee1d8924a335b4fc15296df
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-05-29 11:19:18 +0200

    Update script.js

--- testAPI/script.js
+++ testAPI/script.js
@@ -1,1 +1,96 @@
+let promError = false;
 
+// promise == versprechen
+function getPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("1");
+            if(promError) {
+                reject("hat nicht geklappt")
+            }else {
+                resolve("hat geklappt");
+            }
+        }, 2000);
+    })
+}
+
+function getPromise2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("2");
+            if(!promError) {
+                reject("hat nicht geklappt")
+            }else {
+                resolve("hat geklappt");
+            }
+        }, 2000);
+    })
+}
+
+function getPromise3() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("3");
+            if(promError) {
+                reject("hat nicht geklappt")
+            }else {
+                resolve("hat geklappt");
+            }
+        }, 2000);
+    })
+}
+
+// try catch with async and await
+async function usePromiseTryCatch() {
+    try {
+        await getPromise();
+        await getPromise2();
+        await getPromise3();
+        console.log("Start")
+    } catch (error) {
+        console.error(error);    
+    }
+    console.log("Ende")
+}
+
+// async und await
+async function usePromiseAsyncAwait() {
+    console.log("prom");
+    let prom = await getPromise();
+    console.log("prom2");
+    console.log(prom);
+}
+
+
+// FIREBASE STORAGE
+function getGoodPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("good");
+            reject("done_good");
+        }, 1000);
+    })
+}
+
+function getBadPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("bad");
+            reject("done_bad");
+        }, 1000);
+    })
+}
+
+function usePromise() {
+    getGoodPromise().then(
+        getBadPromise
+    ).then (
+        getGoodPromise
+    ).then (
+        (result) => {
+            console.log(result);
+        }
+    ).catch((error) => {
+        console.error(error);
+    })
+}
